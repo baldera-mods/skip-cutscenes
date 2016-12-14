ead79c29a2454be305358ea0bb521009a26e0e15
meishu <theme97@gmail.com>
2016-12-13 16:54:35 -0800

    Fix permanent desync

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function SkipCutscenes(dispatch) {
-  dispatch.hook('sPlayMovie', function(event) {
-    dispatch.toClient('cEndMovie', Object.assign({ unk: true }, event));
+  dispatch.hook('sPlayMovie', (event) => {
+    dispatch.toServer('cEndMovie', Object.assign({ unk: true }, event));
     return false;
   });
 };
